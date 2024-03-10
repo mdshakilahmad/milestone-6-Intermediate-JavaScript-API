f7faa60f322f2e88ded63838151376d258d13a17
shakil <ahmadshakil22255@gmail.com>
2024-03-10 17:29:49 +0600

    somthing add

--- API-JSON-DataLoad-DynamicWebsite-module-33/js/index.js
+++ API-JSON-DataLoad-DynamicWebsite-module-33/js/index.js
@@ -27,3 +27,43 @@ function displayUsers2(data) {
     ul.appendChild(li);
   }
 }
+
+// module 33-8 start
+
+function deletePost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  });
+}
+
+function patchAPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: "foo",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function createAPoast() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+// module 33-8 end
